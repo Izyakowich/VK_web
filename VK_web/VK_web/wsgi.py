fda608102e3f20cfc1626ba354f7af1e42bdb93d
Izyakowich <andreywillrock@gmail.com>
2023-10-30 01:37:56 +0300

    make pages

---
+++ VK_web/VK_web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for VK_web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VK_web.settings')
+
+application = get_wsgi_application()
